e6198d44ee9b22daa97b6e9be6ef4d6c610fda98
GoeyGreen <114379647+GoeyGreen@users.noreply.github.com>
2025-04-28 23:10:35 -0700

    Added Read from File and displaying strings

--- src/main.rs
+++ src/main.rs
@@ -1,26 +1,34 @@
 
+use std::fs::read_to_string;
+use std::path::PathBuf;
+
+use std::io;
+use std::sync::Arc;
 use iced::time;
 
 use iced::alignment::{Horizontal, Vertical};
 use iced::{application, executor, Element, Length, Settings, Subscription, Task, Theme};
-use iced::widget::{button, column, container, horizontal_space, row, text, text_editor, Container};
+use iced::widget::{button, toggler, column, container, horizontal_space, row, text, text_editor, Container};
 use chrono::{DateTime, Local, prelude};
 
 #[derive(Default, Debug)]
 struct Home {
     enabled: bool,
     num: i32,
+    filters: Vec<String>,
     clock: String,
     time: DateTime<Local>,
+    error: Option<io::ErrorKind>
 }
 
 
 #[derive(Debug, PartialEq, Clone)]
 enum Message {
-    Enable,
+    Enable(bool),
     Tick,
     Increment,
     Decrement,
+    File_Opened(Result<Arc<String>, io::ErrorKind>),
 }
 
 impl Home {
@@ -30,16 +38,27 @@ impl Home {
         Theme::default()
     }
 
-    pub fn new(num: i32 ) -> Self {
-        Home{
-            enabled: false,
-            num: num,
-            clock: prelude::Local::now().format("%d/%m/%Y %H:%M:%S").to_string(),
-            time: prelude::Local::now(),
-        }
+    pub fn new(num: i32, file: String ) -> (Self, Task<Message>) {
+        
+        (   
+            Home {
+                enabled: false,
+                num: num,
+                filters: Vec::new(),
+                clock: prelude::Local::now().format("%d/%m/%Y %H:%M:%S").to_string(),
+                time: prelude::Local::now(),
+                error: None,
+            }, 
+            Task::perform(
+                read_lists(format!("{}/filter.txt", env!("CARGO_MANIFEST_DIR")).into()),
+                Message::File_Opened
+            )
+        )
     }
 
     pub fn view(&self) -> Element<Message>{
+
+
         let default_color = match Home::theme() {
             iced::Theme::Light => iced::Color::BLACK, 
             iced::Theme::Dark => iced::Color::WHITE,
@@ -64,25 +83,36 @@ impl Home {
         let enable_col = column![
             text("Enabled : ".to_owned()).size(16),
             text(if self.enabled {"Y"} else {"N"}).color(if self.enabled {iced::Color::from_rgb(0f32, 255f32, 0f32)} else {iced::Color::from_rgb(255f32, 0f32, 0f32)}),
-            button("Enable").on_press(Message::Enable)
+            toggler(self.enabled).on_toggle(Message::Enable).size(20)
         ].spacing(10)
         .align_x(Horizontal::Center)
         .width(Length::Fill);
 
-        row![
+        let toggles = row![
             counter_col,
             enable_col,
         ].padding(10)
         .spacing(10)
         .align_y(Vertical::Center)
-        .width(Length::Fill).into()
+        .width(Length::Fill);
+
+        let mut window = iced::widget::Column::with_children(vec![
+            toggles.into(),
+        ]).padding(5).align_x(Horizontal::Center);
+
+        for i in &self.filters {
+            window = window.push(text(i).size(16).color(iced::Color::WHITE));
+        }
+
+        
+        window.into()
     
     }
 
     pub fn update(&mut self, message:Message) -> Task<Message>{
         match message {
-            Message::Enable => {
-                self.enabled = !self.enabled;
+            Message::Enable(status) => {
+                self.enabled = status;
             },
             Message::Tick => {
                 if self.time != prelude::Local::now() {
@@ -98,7 +128,14 @@ impl Home {
             },
             Message::Decrement => {
                 self.num -= 1;
+            },
+
+            Message::File_Opened(Ok(content)) => {
+                self.filters.extend(content.as_str().split('\n').map(|x| x.to_owned()));
             }
+            Message::File_Opened(Err(error)) => {
+                self.error = Some(error);
+            } 
 
         };
         Task::none()
@@ -112,7 +149,11 @@ impl Home {
     
 }
 
+async fn read_lists(file: PathBuf) -> Result<Arc<String>, io::ErrorKind> {
+    tokio::fs::read_to_string(file).await.map(Arc::new).map_err(|err| err.kind())
+}
+
 
 fn main() -> iced::Result{
-    iced::application("NetFilterRS", Home::update, Home::view).subscription(Home::subscription).run_with(|| {let initial_state = Home::new(5); (initial_state, iced::Task::none())})
+    iced::application("NetFilterRS", Home::update, Home::view).subscription(Home::subscription).run_with(|| {let initial_state = Home::new(5, "filter.txt".to_owned()); initial_state})
 }
